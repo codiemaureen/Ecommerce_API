921fc0e9840c20d2fc736787abf96cd958e1db8b
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 13:42:04 -0400

    Adding Utils folder

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken');
+const {createJWT} = require('../utils');
 
 
 exports.register = async(req, res) => {
@@ -14,8 +14,9 @@ exports.register = async(req, res) => {
     const isFirstAccount = await User.countDocuments({}) === 0;
     const role = isFirstAccount ? 'admin' : 'user';
     const user = await User.create({name, email, password, role});
-    const tokenUser = {name: user.name, userId: user._id, role: user.role}
-    const token = jwt.sign(tokenUser, 'jwtSecret', {expiresIn: '1d'});
+    const tokenUser = {name: user.name, userId: user._id, role: user.role};
+    const token = createJWT({payload:tokenUser});
+
     res.status(StatusCodes.CREATED).json({ user: tokenUser, token});
 };
 exports.login = async(req, res) => {
---
+++ utils/jwt.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+exports.createJWT = ({payload}) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME,
+    });
+    return token;
+};
+
+exports.isTokenValid = ({token}) => jwt.verify(token, process.env.JWT_SECRET);
