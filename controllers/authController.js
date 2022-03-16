897e7e3b761c496fde992dabc40d1b21b7669e0c
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 13:26:44 -0400

    Adding token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const jwt = require('jsonwebtoken');
+
 
 exports.register = async(req, res) => {
     const {email, name, password} = req.body;
@@ -12,7 +14,9 @@ exports.register = async(req, res) => {
     const isFirstAccount = await User.countDocuments({}) === 0;
     const role = isFirstAccount ? 'admin' : 'user';
     const user = await User.create({name, email, password, role});
-    res.status(StatusCodes.CREATED).json({ user });
+    const tokenUser = {name: user.name, userId: user._id, role: user.role}
+    const token = jwt.sign(tokenUser, 'jwtSecret', {expiresIn: '1d'});
+    res.status(StatusCodes.CREATED).json({ user: tokenUser, token});
 };
 exports.login = async(req, res) => {
     res.send('login')
