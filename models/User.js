bad69e0cb17216da535fe45684e6dc0e13408026
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 12:41:07 -0400

    Hashing password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,5 +32,15 @@ const userSchema = mongoose.Schema({
         default: 'user',
     },
 });
+
+userSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function(canditatePassword){
+    const isMatch = await bcrypt.compare(canditatePassword, this.password);
+    return isMatch;
+}
 
 module.exports = mongoose.model('User', userSchema);
