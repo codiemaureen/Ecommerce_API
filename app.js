e007d7ea1202bfe1f8a5ffcde265c43ac70ee21d
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 11:02:53 -0400

    Adding Middleware

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes/index');
+//database
+const connectDB = require('./db/connect');
 
 //routes
 app.use('/', routes);   
 
-//database
-const connectDB = require('./db/connect');
+//middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+
+
 
 const port = process.env.PORT || 3000;
 const start = async() => {
