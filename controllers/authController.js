38e62575d189c3b3a15fb5a787a3aa8098f10dfb
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 14:18:49 -0400

    Adding cookies to res

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
-const {createJWT} = require('../utils');
+const {attachCookiesToResponse} = require('../utils');
 
 
 exports.register = async(req, res) => {
@@ -15,15 +15,11 @@ exports.register = async(req, res) => {
     const role = isFirstAccount ? 'admin' : 'user';
     const user = await User.create({name, email, password, role});
     const tokenUser = {name: user.name, userId: user._id, role: user.role};
-    const token = createJWT({payload:tokenUser});
-
-    const oneDay = 1000 * 60 * 60 * 24;
-    res.cookie('token', token, {
-        httpOnly:true,
-        expires: new Date(Date.now() + oneDay)
-    })
-    res.status(StatusCodes.CREATED).json({ user: tokenUser, token});
+    attachCookiesToResponse({res, user: tokenUser});
+    res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
+
 exports.login = async(req, res) => {
     res.send('login')
 };
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,9 +1,20 @@
 const jwt = require('jsonwebtoken');
 
-exports.createJWT = ({payload}) => {
+const createJWT = ({payload}) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME,
     });
     return token;
 };
 
 exports.isTokenValid = ({token}) => jwt.verify(token, process.env.JWT_SECRET);
+
+exports.attachCookiesToResponse = ({res, user})  => {
+    const token = createJWT({payload: user});
+    
+    const oneDay = 1000 * 60 * 60 * 24;
+    
+    res.cookie('token', token, {
+        httpOnly:true,
+        expires: new Date(Date.now() + oneDay)
+    });
+};
