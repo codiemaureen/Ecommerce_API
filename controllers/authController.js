a5c4761db5e0ab44569247bfe558363232a1ec3c
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 13:49:28 -0400

    Getting cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,11 @@ exports.register = async(req, res) => {
     const tokenUser = {name: user.name, userId: user._id, role: user.role};
     const token = createJWT({payload:tokenUser});
 
+    const oneDay = 1000 * 60 * 60 * 24;
+    res.cookie('token', token, {
+        httpOnly:true,
+        expires: new Date(Date.now() + oneDay)
+    })
     res.status(StatusCodes.CREATED).json({ user: tokenUser, token});
 };
 exports.login = async(req, res) => {
