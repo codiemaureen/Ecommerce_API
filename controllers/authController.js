e56933625b799820def87c97580dbba13960fc89
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-16 14:48:10 -0400

    Login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,26 @@ exports.register = async(req, res) => {
 
 
 exports.login = async(req, res) => {
-    res.send('login')
+    const { email, password } = req.body;
+
+    if(!email || !password ) {
+        throw new CustomError.BadRequestError('Please provide email and password');
+    };
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new CustomError.UnauthenticatedError('Invalid credentials');
+    };
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if(!isPasswordCorrect){
+        throw new CustomError.UnauthenticatedError('Invalid credentials');
+    };
+    const tokenUser = {name: user.name, userId: user._id, role: user.role};
+    attachCookiesToResponse({res, user: tokenUser});
+    res.status(StatusCodes.CREATED).json({ user: tokenUser })
 };
 exports.logout = async(req, res) => {
     res.send('logout')
