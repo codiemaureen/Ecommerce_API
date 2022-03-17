1dfa0ded8593e2ea473445c5cec007b0dd0d7330
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-17 12:23:32 -0400

    Getting users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,20 @@
 const User = require("../models/User")
+const {StatusCodes} = require('http-status-codes');
+const { CustomAPIError } = require("../errors");
 
 exports.getAllUsers = async (req, res) => {
-    const users = await User.find({});
-    res.send('Get All users');
+    const users = await User.find({role:'user'}).select('-password');
+    res.status(StatusCodes.OK).json({users});
 };
 exports.getSingleUser = async (req, res) => {
-    const users = await User.find({});
-    res.send('Get single user');
+    const user = await User.findOne({_id:req.params.id}).select('-password');
+    if(!user){
+        throw new CustomAPIError.NotFoundError(`No user found with id ${req.params.id}`)
+    };
+        res.status(StatusCodes.OK).json({user});
 };
 exports.showCurrentUser = async (req, res) => {
-    const users = await User.find({});
+    const users = await User.find({id:user_id});
     res.send('Show current user');
 };
 exports.updateUser = async (req, res) => {
