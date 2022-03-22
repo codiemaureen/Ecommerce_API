813ae5bd2dcffac3d2f2e503558ba5e9b0f0fcba
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-22 12:01:57 -0400

    Fixing Check Permissions

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -2,11 +2,11 @@
 const CustomError = require('../errors');
 
 const checKPermissions = (requestUser, resourceUserId) => {
-  if (requestUser.role === 'admin') return;
-  if (requestUser.userId === resourceUserId.toString()) return;
-  throw new CustomError.UnauthorizedError(
-    'Not authorized to access this route'
-  );
+    if (requestUser.role === 'admin') return;
+    if (requestUser.userId === resourceUserId.toString()) return;
+    throw new CustomError.UnauthorizedError(
+        'Not authorized to access this route'
+    );
 };
 
 module.exports = checKPermissions;
