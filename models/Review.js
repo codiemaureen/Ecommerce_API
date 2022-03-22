d851b465b5d837ca265a1cef5c1e639a2d279ce6
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-22 14:24:12 -0400

    Review Model

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: [true, 'Please Provide Rating'],
+    },
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please Provide Title'],
+        maxlength: 100,
+    },
+    comment: {
+        type: String,
+        required: [true, 'Please Provide Comment'],
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref:'User',
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref:'Product',
+        required: true,
+    },
+},
+    {timestamps: true}
+);
+
+//only one review per user
+reviewSchema.index({product:1, user:1}, {unique: true});
+
+model.exports = mongoose.model('Review', reviewSchema);
