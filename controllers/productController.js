2e00bc348b70aebadaa189b09339ed84c948bcb7
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-22 12:57:30 -0400

    Product Controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,19 +10,36 @@ const createProduct = async(req,res) => {
 };
 
 const getAllProducts = async(req,res) => {
-    res.send('get all products');
+    const products = await Product.find({});
+    res.status(StatusCodes.OK).json({products, count: products.length});
 };
 
 const getSingleProduct = async(req,res) => {
-    res.send('get single product');
+    const {id: productId} = req.params;
+    const product = await Product.findOne({_id: productId});
+
+    if(!product){
+        throw new CustomError.NotFoundError(`No product with id: ${productId} found`)
+    }
+    res.status(StatusCodes.OK).json({product});
 };
 
 const updateProduct = async(req,res) => {
-    res.send('update product');
+    const {id: productId} = req.params;
+    const product = await Product.findOneAndUpdate({_id: productId}, req.body, {new: true, runValidators: true});
+    res.status(StatusCodes.OK).json({product})
 };
 
 const deleteProduct = async(req,res) => {
-    res.send('delete product');
+    const {id: productId} = req.params;
+    const product = await Product.findOne({_id: productId});   
+
+    if(!product){
+        throw new CustomError.NotFoundError(`No product with id: ${productId} found`)
+    };
+
+    await product.remove();
+    res.status(StatusCodes.OK).json({msg: `Success! Product has been removed!`})
 };
 
 const uploadImage = async(req,res) => {
