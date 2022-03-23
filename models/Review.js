0c1960cfcee430e49b8ae4ffb771739360e9ed78
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-23 11:55:46 -0400

    Adding Avg Rating Pipeline

--- models/Review.js
+++ models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./Products')
 
 const reviewSchema = new mongoose.Schema({
     rating: {
@@ -36,7 +37,27 @@ reviewSchema.index({product:1, user:1}, {unique: true});
 
 
 reviewSchema.statics.calculateAverageRating = async function(productId) {
-    console.log(productId);;
+    const result = await this.aggregate([
+        {$match: {product: productId}},
+        {$group: {
+            _id: null, 
+            averageRating: {$avg: '$rating'},
+            numOfReviews: { $sum: 1},
+            },
+        },
+    ]);
+    console.log(result);
+    try {
+        await this.model('Product').findOneAndUpdate(
+            {_id:productId}, 
+            {
+                averageRating: Math.ceil(result[0]?.averageRating || 0),
+                numOfReviews: result[0]?.numOfReviews || 0,
+            },
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 reviewSchema.post('save', async function(){
