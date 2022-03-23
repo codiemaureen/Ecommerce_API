b09586b97021eb1fd57c5ec4e148bc2be1d7fc6c
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-23 10:48:03 -0400

    Aggregate Setup

--- models/Review.js
+++ models/Review.js
@@ -31,9 +31,19 @@ const reviewSchema = new mongoose.Schema({
     {timestamps: true}
 );
 
-
-
 //only one review per user
 reviewSchema.index({product:1, user:1}, {unique: true});
+
+
+reviewSchema.statics.calculateAverageRating = async function(productId) {
+    console.log(productId);;
+};
+
+reviewSchema.post('save', async function(){
+    await this.constructor.calculateAverageRating(this.product);
+});
+reviewSchema.post('remove', async function(){
+    await this.constructor.calculateAverageRating(this.product);
+});
 
 module.exports = mongoose.model('Review', reviewSchema);
